825eb2fcebd5cc60154f69a1943cc0051b6d050b
Steven Halim <stevenhalim@gmail.com>
2018-11-30 15:35:48 +0800

    Update two code

--- ch3/14_UVa00108.cpp
+++ ch3/14_UVa00108.cpp
@@ -17,14 +17,11 @@ int main() {     // O(n^3) 1D DP + greedy (Kadane's) solution, 0.008 s in UVa
   for (int l = 0; l < n; l++) for (int r = l; r < n; r++) {
     int subRect = 0;
     for (int row = 0; row < n; row++) {
-      // Max 1D Range Sum on columns of this row i
-      if (l > 0) subRect += A[row][r] - A[row][l-1];
-      else       subRect += A[row][r];
-      // Kadane's algorithm on rows
+      if (l > 0) subRect += A[row][r] - A[row][l-1];      // Max 1D Range Sum
+      else       subRect += A[row][r];              // on columns of this row
       if (subRect < 0) subRect = 0;     // greedy, restart if running sum < 0
-      maxSubRect = max(maxSubRect, subRect);
-    }
-  }
+      maxSubRect = max(maxSubRect, subRect);    // Kadane's algorithm on rows
+  } }
   printf("%d\n", maxSubRect);
   return 0;
 }
@@ -42,7 +39,7 @@ int main() {                            // O(n^4) DP solution, ~0.076s in UVa
   int maxSubRect = -127*100*100;                 // the lowest possible value
   for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) // start coordinate
     for (int k = i; k < n; k++) for (int l = j; l < n; l++) {    // end coord
-      subRect = A[k][l];      // sum of all items from (0, 0) to (k, l): O(1)
+      int subRect = A[k][l];  // sum of all items from (0, 0) to (k, l): O(1)
       if (i > 0) subRect -= A[i-1][l];                                // O(1)
       if (j > 0) subRect -= A[k][j-1];                                // O(1)
       if (i > 0 && j > 0) subRect += A[i-1][j-1];                     // O(1)
@@ -65,7 +62,8 @@ int main() {                // O(n^6) brute force solution, TLE (> 3s) in UVa
       int subRect = 0;                     // sum items in this sub-rectangle
       for (int a = i; a <= k; a++) for (int b = j; b <= l; b++)
         subRect += A[a][b];
-      maxSubRect = max(maxSubRect, subRect); }          // the answer is here
+      maxSubRect = max(maxSubRect, subRect);            // the answer is here
+    }
   printf("%d\n", maxSubRect);
   return 0;
 }
--- ch3/18_UVa10496.cpp
+++ ch3/18_UVa10496.cpp
@@ -1,43 +1,36 @@
 // Collecting Beepers
-// DP TSP
 
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 
-int i, j, TC, xsize, ysize, n, x[11], y[11], dist[11][11], memo[11][1 << 11]; // Karel + max 10 beepers
+#define MAX_n 11
 
-int tsp(int pos, int bitmask) { // bitmask stores the visited coordinates
-  if (bitmask == (1 << (n + 1)) - 1)
-    return dist[pos][0]; // return trip to close the loop
-  if (memo[pos][bitmask] != -1)
-    return memo[pos][bitmask];
+int n, dist[MAX_n][MAX_n], memo[MAX_n][1<<(MAX_n-1)]; // Karel + max 10 beepers
 
+int tsp(int c, int mask) {           // mask stores the visited coordinates
+  if (mask == (1<<n)-1) return dist[c][0];  // return to close the loop
+  if (memo[c][mask] != -1) return memo[c][mask];
   int ans = 2000000000;
-  for (int nxt = 0; nxt <= n; nxt++) // O(n) here
-    if (nxt != pos && !(bitmask & (1 << nxt))) // if coordinate nxt is not visited yet
-      ans = min(ans, dist[pos][nxt] + tsp(nxt, bitmask | (1 << nxt)));
-  return memo[pos][bitmask] = ans;
+  for (int nxt = 1; nxt <= n; nxt++) // O(n) here
+    if (!(mask & (1<<(nxt-1)))) // if coordinate nxt is not visited yet
+      ans = min(ans, dist[c][nxt] + tsp(nxt, mask | (1<<(nxt-1))));
+  return memo[c][mask] = ans;
 }
 
 int main() {
-  scanf("%d", &TC);
+  int TC; scanf("%d", &TC);
   while (TC--) {
-    scanf("%d %d", &xsize, &ysize); // these two values are not used
+    int xsize, ysize; scanf("%d %d", &xsize, &ysize); // these two values are not used
+    int x[11], y[11];
     scanf("%d %d", &x[0], &y[0]);
     scanf("%d", &n);
-    for (i = 1; i <= n; i++) // karel's position is at index 0
+    for (int i = 1; i <= n; i++) // karel's cition is at index 0
       scanf("%d %d", &x[i], &y[i]);
-
-    for (i = 0; i <= n; i++) // build distance table
-      for (j = 0; j <= n; j++)
-        dist[i][j] = abs(x[i] - x[j]) + abs(y[i] - y[j]); // Manhattan distance
-
+    for (int i = 0; i <= n; i++) // build distance table
+      for (int j = 0; j <= n; j++)
+        dist[i][j] = abs(x[i]-x[j]) + abs(y[i]-y[j]); // Manhattan distance
     memset(memo, -1, sizeof memo);
-    printf("The shortest path has length %d\n", tsp(0, 1)); // DP-TSP
+    printf("The shortest path has length %d\n", tsp(0, 0)); // DP-TSP
   }
-
   return 0;
 }
