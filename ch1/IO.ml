e3007638ccd0c5d15104cc2b10745df6841ea92b
Bay Wei Heng <bayweiheng@gmail.com>
2018-12-16 10:48:07 +0800

    fix bugs in FormingQuizTeams.ml, standardised to use snake instead of camelcase, add IO.ml

---
+++ ch1/IO.ml
@@ -0,0 +1,84 @@
+open Scanf
+open Printf
+
+let () =
+  (* comment all lines and only uncomment demo code that you are interested with *)
+
+  let ic = Scanning.open_in "04_in1.txt" in 
+  bscanf ic "%d\n" (fun tc ->
+    for i = 1 to tc do 
+      bscanf ic "%d %d\n" (fun a b ->
+        printf "%d\n" (a+b)
+      )
+    done
+  )
+
+(*   let ic = Scanning.open_in "04_in2.txt" in
+  let rec process () =
+    bscanf ic "%d %d\n" (fun a b ->
+      if a = 0 && b = 0 then ()
+      else begin
+        printf "%d\n" (a+b);
+        process ()
+      end
+    )
+  in 
+  process () *)
+
+(*   let ic = Scanning.open_in "04_in3.txt" in 
+  try
+    let case = ref 1 in
+    while true do 
+      bscanf ic "%d %d\n" (fun a b ->
+        printf "Case %d: %d\n\n" !case (a+b);
+        incr case
+      )
+    done
+  with End_of_file ->
+    () *)
+
+(*   let ic = Scanning.open_in "04_in3.txt" in 
+  try
+    let case = ref 1 in
+    while true do 
+      bscanf ic "%d %d\n" (fun a b ->
+        if !case > 1 then printf "\n";
+        printf "Case %d: %d\n" !case (a+b);
+        incr case
+      )
+    done
+  with End_of_file ->
+    () *)
+
+(*   let ic = Scanning.open_in "04_in4.txt" in 
+  try
+    while true do 
+      bscanf ic "%d" (fun k ->
+        let ans = ref 0 in
+        for i = 1 to k do 
+          bscanf ic " %d" (fun v -> ans := !ans + v) 
+        done;
+        bscanf ic "\n" ();
+        printf "%d\n" !ans
+      )
+    done
+  with End_of_file ->
+    () *)
+
+(*   let ic = Scanning.open_in "04_in5.txt" in 
+  let rec sum_line acc =
+    bscanf ic "%d%c" (fun v dummy ->
+      if dummy = '\n' then printf "%d\n" (acc+v)
+      else sum_line (acc+v)
+    )
+  in
+  try
+    while true do 
+      sum_line 0
+    done
+  with End_of_file ->
+    () *)
+
+
+
+
