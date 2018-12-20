c3e572ab1c4a76fec15a2db31048fe963814b016
How Si Wei <git@howsiwei.me>
2018-12-20 20:09:46 +0800

    Open Seq

--- ch2/06_map_set.ml
+++ ch2/06_map_set.ml
@@ -1,12 +1,13 @@
 open Printf
+open Seq
 
 module ISet = Set.Make (struct type t = int let compare = compare end)
 module SMap = Map.Make (struct type t = string let compare = compare end)
 
 let rec take_while pred seq () =
   match seq() with
-  | Seq.Nil -> Seq.Nil
-  | Seq.Cons (x, next) -> if pred x then Cons (x, take_while pred next) else Seq.Nil
+  | Nil -> Nil
+  | Cons (x, next) -> if pred x then Cons (x, take_while pred next) else Nil
 
 let () =
   let mapper = [
@@ -18,7 +19,7 @@ let () =
       ("grace", 75);
       ("martin", 81);
     ] |> List.to_seq |> SMap.of_seq in
-  let used_values = SMap.to_seq mapper |> Seq.map snd |> ISet.of_seq in
+  let used_values = SMap.to_seq mapper |> map snd |> ISet.of_seq in
 
   SMap.iter (fun k v -> printf "%s %d\n" k v) mapper;
 
@@ -27,15 +28,15 @@ let () =
   printf "==================\n";
 
   SMap.to_seq_from "f" mapper |> take_while (fun (k, _) -> k < "m")
-  |> Seq.iter (fun (k, v) -> printf "%s %d\n" k v);
+  |> iter (fun (k, v) -> printf "%s %d\n" k v);
 
   printf "%d\n" (ISet.find 77 used_values);
 
   ISet.to_seq used_values |> take_while (fun x -> x < 77)
-  |> Seq.iter (printf "%d,");
+  |> iter (printf "%d,");
   printf "\n";
 
-  ISet.to_seq_from 77 used_values |> Seq.iter (printf "%d,");
+  ISet.to_seq_from 77 used_values |> iter (printf "%d,");
   printf "\n";
 
   match ISet.find_opt 79 used_values with
