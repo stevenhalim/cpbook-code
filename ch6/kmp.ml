315f981446870541ea37a6251dd5b59ceb207649
Phan Duc Nhat Minh <phanducnhatminh@gmail.com>
2018-12-22 18:09:49 +0800

    KMP

---
+++ ch6/kmp.ml
@@ -0,0 +1,36 @@
+let failure_function pattern =
+  let m = String.length pattern in
+  let fail = Array.make (m + 1) (-1) in
+  let i, j = ref 0, ref (-1) in
+  while !i < m do
+    while !j >= 0 && pattern.[!i] != pattern.[!j] do
+      j := fail.(!j)
+    done;
+    incr i; incr j;
+    fail.(!i) <- !j
+  done;
+  fail
+
+let kmp_search text pattern fail =
+  let n = String.length text in
+  let m = String.length pattern in
+  let i, j = ref 0, ref 0 in
+  let matches = ref [] in
+  while !i < n do
+    while !j >= 0 && text.[!i] != pattern.[!j] do
+      j := fail.(!j)
+    done;
+    incr i; incr j;
+    if !j = m then begin
+      matches := !i - !j :: !matches;
+      j := fail.(!j)
+    end
+  done;
+  !matches |> List.rev
+
+let () =
+  let text = "I DO NOT LIKE SEVENTY SEV BUT SEVENTY SEVENTY SEVEN" in
+  let pattern = "SEVENTY SEVEN" in
+  let fail = failure_function pattern in
+  kmp_search text pattern fail
+    |> List.iter (Printf.printf "P is found at index %d in T\n")
