b0b3b77875269d2f0a2f76ebe6dcf2d47a348ed1
Steven Halim <stevenhalim@gmail.com>
2019-03-07 10:31:11 +0800

    update comment style and pre-increment

--- ch6/sa.cpp
+++ ch6/sa.cpp
@@ -3,107 +3,109 @@ using namespace std;
 
 typedef pair<int, int> ii;
 
-#define MAX_N 100010                         // second approach: O(n log n)
+const int MAX_N = 100010;                        // up to 100K characters
 
-char T[MAX_N];                   // the input string, up to 100K characters
-int n;                                        // the length of input string
-int RA[MAX_N], tempRA[MAX_N];        // rank array and temporary rank array
-int SA[MAX_N], tempSA[MAX_N];    // suffix array and temporary suffix array
+char T[MAX_N];                                   // the input string
+int n;                                           // the length of T
+int RA[MAX_N], tempRA[MAX_N];                    // rank array
+int SA[MAX_N], tempSA[MAX_N];                    // suffix array
 int c[MAX_N];                                    // for counting/radix sort
 
-char P[MAX_N];                  // the pattern string (for string matching)
-int m;                                      // the length of pattern string
+char P[MAX_N];                                   // the pattern string
+int m;                                           // the length of P
 
-int Phi[MAX_N];                      // for computing longest common prefix
+int Phi[MAX_N];                                  // for computing LCP
 int PLCP[MAX_N];
-int LCP[MAX_N];  // LCP[i] stores the LCP between previous suffix T+SA[i-1]
-                                              // and current suffix T+SA[i]
+// LCP[i] = the LCP between previous suffix T+SA[i-1] and suffix T+SA[i]
+int LCP[MAX_N];
 
-bool cmp(int a, int b) { return strcmp(T+a, T+b) < 0; }          // compare
+bool cmp(int a, int b) { return strcmp(T+a, T+b) < 0; } // compare
 
-void constructSA_slow() {               // cannot go beyond 1000 characters
-  for (int i = 0; i < n; i++) SA[i] = i; // initial SA: {0, 1, 2, ..., n-1}
-  sort(SA, SA+n, cmp);   // sort: O(n log n) * compare: O(n) = O(n^2 log n)
+void constructSA_slow() {                        // max ~1000 characters
+  for (int i = 0; i < n; ++i) SA[i] = i;         // this is the initial SA
+  sort(SA, SA+n, cmp); // sort: O(n log n) * compare: O(n) = O(n^2 log n)
 }
 
-void countingSort(int k) {                                          // O(n)
-  int i, sum, maxi = max(300, n);   // up to 255 ASCII chars or length of n
-  memset(c, 0, sizeof c);                          // clear frequency table
-  for (i = 0; i < n; i++)       // count the frequency of each integer rank
-    c[i+k < n ? RA[i+k] : 0]++;
-  for (i = sum = 0; i < maxi; i++) {
+void countingSort(int k) {                       // O(n)
+  int i, sum, maxi = max(300, n);                // up to 255 ASCII chars
+  memset(c, 0, sizeof c);                        // clear frequency table
+  for (i = 0; i < n; ++i)                        // count the frequency
+    ++c[i+k < n ? RA[i+k] : 0];                  // of each integer rank
+  for (i = sum = 0; i < maxi; ++i) {
     int t = c[i]; c[i] = sum; sum += t;
   }
-  for (i = 0; i < n; i++)          // shuffle the suffix array if necessary
+  for (i = 0; i < n; ++i)                        // shuffle SA
     tempSA[c[SA[i]+k < n ? RA[SA[i]+k] : 0]++] = SA[i];
-  for (i = 0; i < n; i++)                     // update the suffix array SA
+  for (i = 0; i < n; ++i)                        // update SA
     SA[i] = tempSA[i];
 }
 
-void constructSA() {         // this version can go up to 100000 characters
+void constructSA() {                             // can go up to 100K chars
   int i, k, r;
-  for (i = 0; i < n; i++) RA[i] = T[i];                 // initial rankings
-  for (i = 0; i < n; i++) SA[i] = i;     // initial SA: {0, 1, 2, ..., n-1}
-  for (k = 1; k < n; k <<= 1) {       // repeat sorting process log n times
-    countingSort(k);  // actually radix sort: sort based on the second item
-    countingSort(0);          // then (stable) sort based on the first item
-    tempRA[SA[0]] = r = 0;             // re-ranking; start from rank r = 0
-    for (i = 1; i < n; i++)                    // compare adjacent suffixes
+  for (i = 0; i < n; ++i) RA[i] = T[i];          // initial rankings
+  for (i = 0; i < n; ++i) SA[i] = i;             // initial SA
+  for (k = 1; k < n; k <<= 1) {                  // repeat log n times
+    // this is actually radix sort: sort based on the second item
+    // and then (stable) sort based on the first item
+    countingSort(k);                             
+    countingSort(0);
+    tempRA[SA[0]] = r = 0;                       // re-ranking process
+    for (i = 1; i < n; ++i)                      // compare adj suffixes
       tempRA[SA[i]] = // if same pair => same rank r; otherwise, increase r
       (RA[SA[i]] == RA[SA[i-1]] && RA[SA[i]+k] == RA[SA[i-1]+k]) ? r : ++r;
-    for (i = 0; i < n; i++)                     // update the rank array RA
+    for (i = 0; i < n; ++i)                      // update RA
       RA[i] = tempRA[i];
-    if (RA[SA[n-1]] == n-1) break;               // nice optimization trick
-} }
+    if (RA[SA[n-1]] == n-1) break;               // nice optimization
+  }
+}
 
 void computeLCP_slow() {
-  LCP[0] = 0;                                              // default value
-  for (int i = 1; i < n; i++) {                // compute LCP by definition
-    int L = 0;                                       // always reset L to 0
-    while (T[SA[i]+L] == T[SA[i-1]+L]) L++;          // same L-th char, L++
+  LCP[0] = 0;                                    // default value
+  for (int i = 1; i < n; ++i) {                  // compute LCP by def
+    int L = 0;                                   // always reset L to 0
+    while (T[SA[i]+L] == T[SA[i-1]+L]) ++L;      // same L-th char, L++
     LCP[i] = L;
-} }
+  }
+}
 
 void computeLCP() {
   int i, L;
-  Phi[SA[0]] = -1;                                         // default value
-  for (i = 1; i < n; i++)                            // compute Phi in O(n)
-    Phi[SA[i]] = SA[i-1];    // remember which suffix is behind this suffix
-  for (i = L = 0; i < n; i++) {             // compute Permuted LCP in O(n)
-    if (Phi[i] == -1) { PLCP[i] = 0; continue; }            // special case
-    while (T[i+L] == T[Phi[i]+L]) L++;           // L increased max n times
+  Phi[SA[0]] = -1;                               // default value
+  for (i = 1; i < n; ++i)                        // compute Phi in O(n)
+    Phi[SA[i]] = SA[i-1];                        // remember prev suffix
+  for (i = L = 0; i < n; ++i) {                  // compute PLCP in O(n)
+    if (Phi[i] == -1) { PLCP[i] = 0; continue; } // special case
+    while (T[i+L] == T[Phi[i]+L]) L++;           // L incr max n times
     PLCP[i] = L;
-    L = max(L-1, 0);                             // L decreased max n times
+    L = max(L-1, 0);                             // L dec max n times
   }
-  for (i = 0; i < n; i++)                            // compute LCP in O(n)
-    LCP[i] = PLCP[SA[i]];   // put the permuted LCP to the correct position
+  for (i = 0; i < n; ++i)                        // compute LCP in O(n)
+    LCP[i] = PLCP[SA[i]];                        // restore PLCP
 }
 
-ii stringMatching() {                      // string matching in O(m log n)
-  int lo = 0, hi = n-1, mid = lo;              // valid matching = [0..n-1]
-  while (lo < hi) {                                     // find lower bound
-    mid = (lo+hi) / 2;                                // this is round down
-    int res = strncmp(T+SA[mid], P, m);    // try to find P in suffix 'mid'
-    if (res >= 0) hi = mid;        // prune upper half (notice the >= sign)
-    else          lo = mid+1;             // prune lower half including mid
-  }                                      // observe `=' in "res >= 0" above
-  if (strncmp(T+SA[lo], P, m) != 0) return ii(-1, -1);      // if not found
+ii stringMatching() {                            // in O(m log n)
+  int lo = 0, hi = n-1, mid = lo;                // range = [0..n-1]
+  while (lo < hi) {                              // find lower bound
+    mid = (lo+hi) / 2;                           // this is round down
+    int res = strncmp(T+SA[mid], P, m);          // find P in suffix 'mid'
+    (res >= 0) hi = mid ? lo = mid+1;            // notice the >= sign
+  }
+  if (strncmp(T+SA[lo], P, m) != 0) return ii(-1, -1); // if not found
   ii ans; ans.first = lo;
   lo = 0; hi = n-1; mid = lo;
-  while (lo < hi) {            // if lower bound is found, find upper bound
+  while (lo < hi) {                              // now find upper bound
     mid = (lo+hi) / 2;
     int res = strncmp(T+SA[mid], P, m);
-    if (res > 0) hi = mid;                              // prune upper half
-    else         lo = mid+1;              // prune lower half including mid
-  }                           // (notice the selected branch when res == 0)
-  if (strncmp(T+SA[hi], P, m) != 0) hi--;                   // special case
+    (res > 0) hi = mid ? lo = mid+1;             // notice the > sign
+  }
+  if (strncmp(T+SA[hi], P, m) != 0) --hi;        // special case
   ans.second = hi;
   return ans;
 } // return lower/upperbound as first/second item of the pair, respectively
 
-ii LRS() {                 // returns a pair (the LRS length and its index)
+ii LRS() {  // returns a pair (the LRS length and its index)
   int i, idx = 0, maxLCP = -1;
-  for (i = 1; i < n; i++)                         // O(n), start from i = 1
+  for (i = 1; i < n; ++i)                        // O(n), start from i = 1
     if (LCP[i] > maxLCP)
       maxLCP = LCP[i], idx = i;
   return ii(maxLCP, idx);
@@ -113,7 +115,7 @@ int owner(int idx) { return (idx < n-m-1) ? 1 : 2; }
 
 ii LCS() {                 // returns a pair (the LCS length and its index)
   int i, idx = 0, maxLCP = -1;
-  for (i = 1; i < n; i++)                         // O(n), start from i = 1
+  for (i = 1; i < n; ++i)                         // O(n), start from i = 1
     if (owner(SA[i]) != owner(SA[i-1]) && LCP[i] > maxLCP)
       maxLCP = LCP[i], idx = i;
   return ii(maxLCP, idx);
