ed3e81e4f6577ef7b550ff073d92920c59c1f889
zj-cs2103 <zj.cs2103@gmail.com>
2019-03-10 10:06:36 +0800

    ch4: add bellman-ford.py

---
+++ ch4/bellman_ford.py
@@ -0,0 +1,48 @@
+def main():
+	INF = int(1e9)
+
+	# Graph in Figure 4.18, has negative weight, but no negative cycle
+	# 5 5 0
+	# 0 1 1
+	# 0 2 10
+	# 1 3 2
+	# 2 3 -10
+	# 3 4 3
+	# Graph in Figure 4.19, negative cycle exists
+	# 3 3 0
+	# 0 1 1000
+	# 1 2 15
+	# 2 1 -42
+
+	f = open("bellman_ford_in.txt", "r")
+
+	V, E, s = map(int, f.readline().split(" "))
+	AL = [[] for i in range(V)]
+	for i in range(E):
+		u, v, w = map(int, f.readline().split(" "))
+		AL[u].append((v, w))
+
+	# Bellman Ford's routine, basically = relax all E edges V-1 times
+	dist = [INF for i in range(V)]
+	dist[s] = 0
+	for i in range(0, V-1):										# total O(V*E)
+		for u in range(0, V):									# these two loops = O(E)
+			if (not dist[u] == INF):							# important check
+				for v, w in AL[u]:
+					dist[v] = min(dist[v], dist[u] + w)
+
+	hasNegativeCycle = False
+	for u in range(0, V):										# one more pass to check
+		if (not dist[u] == INF):
+			for v, w in AL[u]:
+				if (dist[v] > dist[u] + w):						# should be false
+					hasNegativeCycle = True						# if true => -ve cycle
+
+	print("Negative Cycle Exist? {}".format("Yes" if hasNegativeCycle else "No"))
+
+	if (not hasNegativeCycle):
+		for i in range(0, V):
+			print("SSSP({}, {}) = {}".format(s, i, dist[i]))
+
+
+main()
