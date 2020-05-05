6418a4ce7c8c9681511e4d18d471fe470e01f839
Steven Halim <stevenhalim@gmail.com>
2020-05-05 22:20:35 +0800

    fix indentation

--- ch5/UVa10814.py
+++ ch5/UVa10814.py
@@ -1,11 +1,11 @@
 # 0.040s in Python
 
-from fractions import Fraction		# Python's built in fractions module
+from fractions import Fraction    # Python's built in fractions module
 
 def main():
-	N = int(input())
-	for _ in range(N):
-		frac = Fraction("".join(input().split(" ")))
-		print(str(frac.numerator) + " / " + str(frac.denominator))
+    N = int(input())
+    for _ in range(N):
+        frac = Fraction("".join(input().split(" ")))
+        print(str(frac.numerator) + " / " + str(frac.denominator))
 
 main()
