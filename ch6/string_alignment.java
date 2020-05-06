251c24951d8f473036e259e37a164ea11b94cd11
Steven Halim <stevenhalim@gmail.com>
2020-05-06 12:21:32 +0800

    rename files

--- ch6/string_alignment.java
+++ ch6/string_alignment.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-class ch6_03_str_align {
+class string_alignment {
   public static void main(String[] args){
     char[] A = "ACAATCC".toCharArray(), B = "AGCATGC".toCharArray();
     int[][] table = new int[20][20]; // Needleman Wunsnch's algorithm
-    int i, j, n = A.length, m = B.length;
+    int n = A.length, m = B.length;
 
     // insert/delete = -1 point
-    for (i = 1; i <= n; i++)
+    for (int i = 1; i <= n; ++i)
       table[i][0] = i * -1;
-    for (j = 1; j <= m; j++)
+    for (int j = 1; j <= m; ++j)
       table[0][j] = j * -1;
 
-    for (i = 1; i <= n; i++)
-      for (j = 1; j <= m; j++) {
+    for (int i = 1; i <= n; ++i)
+      for (int j = 1; j <= m; ++j) {
         // match = 2 points, mismatch = -1 point
         table[i][j] = table[i - 1][j - 1] + (A[i - 1] == B[j - 1] ? 2 : -1); // cost for match or mismatches
         // insert/delete = -1 point
@@ -22,8 +22,8 @@ class ch6_03_str_align {
       }
 
     System.out.printf("DP table:\n");
-    for (i = 0; i <= n; i++) {
-      for (j = 0; j <= m; j++)
+    for (int i = 0; i <= n; ++i) {
+      for (int j = 0; j <= m; ++j)
         System.out.printf("%3d", table[i][j]);
       System.out.printf("\n");
     }
