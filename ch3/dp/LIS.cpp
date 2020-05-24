1f0365ed9f9259dab450107f3a484e92067d8d55
Steven Halim <stevenhalim@gmail.com>
2020-05-25 00:04:10 +0800

    fix minor bug at LIS.cpp

--- ch3/dp/LIS.cpp
+++ ch3/dp/LIS.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 typedef vector<int> vi;
 
-const int MAX_N = 100010;
-
-int n = 12, A[] = {-7, 10, 9, 2, 3, 8, 8, 1, 2, 3, 4, 99};
+int n;
+vi A;
 
 void print_array(const char *s, vi &L, int n) {
   for (int i = 0; i < n; ++i) {
@@ -24,13 +23,13 @@ void print_LIS(int i) {                          // backtracking routine
   printf(" %d", A[i]);
 }
 
-int memo[MAX_N];                                 // MAX_N up to 10^4
+int memo[10010];                                 // old limit: up to 10^4
 
 int LIS(int i) {                                 // O(n^2) overall
   if (i == 0) return 1;
   int &ans = memo[i];
   if (ans != -1) return ans;                     // was computed before
-  ans = 0;
+  ans = 1;                                       // LIS can start anywhere
   for (int j = 0; j < i; ++j)                    // O(n) here
     if (A[j] < A[i])                             // increasing condition
       ans = max(ans, LIS(j)+1);                  // pick the max
@@ -38,11 +37,30 @@ int LIS(int i) {                                 // O(n^2) overall
 }
 
 int main() {
+  // note: A[n-1] must be set as the largest value ("INF")
+  // so that all LIS (that can start anywhere) will end at n-1
+  srand(time(NULL));
+  int n = 10+rand()%11;                          // [10..20]
+  A.assign(n, 0);
+  A[n-1] = 99;                                   // set A[n-1] = INF
+  for (int i = 0; i < n-1; ++i)
+    A[i] = rand()%101-50;                        // [-50..50]
+
+  n = 12;
+  vi sample({-7, 10, 9, 2, 3, 8, 8, 1, 2, 3, 4, 99});
+  A = sample;
+
+  printf("n = %d:", n);
+  for (int i = 0; i < n; ++i)
+    printf(" %d", A[i]);
+  printf("\n");
+
   // early 2000 problems usually accept O(n^2) solution
   memset(memo, -1, sizeof memo);
   printf("LIS length is %d\n\n", LIS(n-1));      // with O(n^2) DP
 
   // 2020s problems will likely only accept O(n log k) solution
+  // new limit: n can be up to 200K
   int k = 0, lis_end = 0;
   vi L(n, 0), L_id(n, 0);
   p.assign(n, -1);
@@ -68,5 +86,7 @@ int main() {
 
   printf("Final LIS is of length %d: ", k);
   print_LIS(lis_end); printf("\n");
+
+  assert(LIS(n-1) == k);                         // both must be identical
   return 0;
 }
