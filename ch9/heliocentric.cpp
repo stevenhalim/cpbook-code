50ebf7e8c14ca117df125fc168de3424b94b82fa
How Si Wei <git@howsiwei.me>
2020-07-12 14:43:20 +0800

    Add heliocentric.ml and fix style issues in cpp version

--- ch9/heliocentric.cpp
+++ ch9/heliocentric.cpp
@@ -15,9 +15,9 @@ int extEuclid(int a, int b, int &x, int &y) {    // pass x and y by ref
   int yy = x = 1;
   while (b) {                                    // repeats until b == 0
     int q = a/b;
-    int t = b; b = a%b; a = t;
-    t = xx; xx = x-q*xx; x = t;
-    t = yy; yy = y-q*yy; y = t;
+    tie(a, b) = tuple(b, a%b);
+    tie(x, xx) = tuple(xx, x-q*xx);
+    tie(y, yy) = tuple(yy, y-q*yy);
   }
   return a;                                      // returns gcd(a, b)
 }
@@ -32,7 +32,7 @@ int modInverse(int b, int m) {                   // returns b^(-1) (mod m)
 
 int crt(vi r, vi m) {
   // m_t = m_0*m_1*...*m_{n-1}
-  int mt = accumulate(m.begin(), m.end(), 1, multiplies<int>());
+  int mt = accumulate(m.begin(), m.end(), 1, multiplies<>());
   int x = 0;
   for (int i = 0; i < (int)m.size(); ++i) {
     int a = mod((ll)r[i] * modInverse(mt/m[i], m[i]), m[i]);
