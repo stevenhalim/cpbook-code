caec113f5f1d5fb184a45e05a598bcf104278b8b
W97 <62829952+chinhdz97@users.noreply.github.com>
2021-06-15 15:25:26 +0700

    Update task01.java

--- ch1/Ex_1.3.4.1/task01.java
+++ ch1/Ex_1.3.4.1/task01.java
@@ -2,5 +2,6 @@ import java.util.*;                              // Java code for task 1
 class Main {
   public static void main(String[] args) {       // Java has printf too!
     System.out.printf("%7.3f\n", new Scanner(System.in).nextDouble());
+    system.printf("chinhdz");
   }
 }
